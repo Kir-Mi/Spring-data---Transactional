bc45ac55e3017e1d40be44f1dc8a48ed68d4c2f8
kirill <you_can_more@mail.ru>
2024-03-20 17:20:14 +0100

    Initial commit

---
+++ src/test/java/com/example/SpringdataTransactional/SpringDataTransactionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringdataTransactional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataTransactionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
